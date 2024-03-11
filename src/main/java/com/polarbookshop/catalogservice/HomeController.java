109cf701209a007851f789548f074d32a82b9476
brayandiaz <brayandiaz@2806-102e-0016-0ef0-ca39-cc57-6c03-dbf0.ipv6.infinitum.net.mx>
2024-03-10 22:06:07 -0600

    cambios

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -2,12 +2,13 @@ package com.polarbookshop.catalogservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String getGreeting()
-    {
-        return "Welcome to the book catalog!";
-    }
+	@GetMapping("/")
+	public String getGreeting() {
+		return "Welcome to the book catalog!";
+	}
+
 }
